1f8831813e5a864609a4581c080fd92d8693e48a
novy213 <j.solarek@wp.pl>
2023-08-06 19:29:19 +0200

    fixed login layout

--- javascript/loginJS.js
+++ javascript/loginJS.js
@@ -1,65 +1,43 @@
-const { to, set, timeline, registerPlugin } = gsap
-
-registerPlugin(MorphSVGPlugin)
-
-document.querySelectorAll('.password-field').forEach(field => {
-    let input = field.querySelectorAll('input'),
-        button = field.querySelector('button'),
-        time = timeline({
-            paused: true
-        }).to(field.querySelector('svg .top'), {
-            morphSVG: 'M2 10.5C2 10.5 6.43686 15.5 10.5 15.5C14.5631 15.5 19 10.5 19 10.5',
-            duration: .2
-        }).to(field, {
-            keyframes: [{
-                '--eye-s': 0,
-                '--eye-background': 1,
-                duration: .2
-            }, {
-                '--eye-offset': '0px',
-                duration: .15
-            }]
-        }, 0)
-    button.addEventListener('click', e => {
-        if(field.classList.contains('show')) {
-            field.classList.remove('show')
-            time.reverse(0)
-            return
-        }
-        field.classList.add('show')
-        time.play(0)
-    })
-    field.addEventListener('pointermove', e => {
-        const rect = button.getBoundingClientRect()
-        const fullWidth = rect.width
-        const halfWidth = fullWidth / 2
-        const fullHeight = rect.height
-        const halfHeight = fullHeight / 2
-        let x = e.clientX - rect.left - halfWidth,
-            y = e.clientY - rect.top - halfHeight
-
-        field.style.setProperty('--eye-x', (x < -halfWidth ? -halfWidth : x > fullWidth ? fullWidth : x) / 15 + 'px')
-        field.style.setProperty('--eye-y', (y < -halfHeight ? -halfHeight : y > fullHeight ? fullHeight : y) / 25 + 'px')
-    })
-    field.addEventListener('pointerleave', e => {
-        field.style.setProperty('--eye-x', '0px')
-        field.style.setProperty('--eye-y', '0px')
-    })
-    input.forEach(single => single.addEventListener('input', e => input.forEach(i => i.value = e.target.value)))
-})
 const regex = new RegExp(
     '^(([^<>()[\\]\\\\.,;:\\s@\\"]+(\\.[^<>()[\\]\\\\.,;:\\s@\\"]+)*)|' +
     '(\\".+\\"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])' +
     '|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$'
 );
-
-document.querySelectorAll('.email-input').forEach(container => {
-
-    let input = container.querySelector('input')
-
-    input.addEventListener('keyup', e => container.classList.toggle('valid', regex.test(input.value)))
-
-});
+function Eye(){
+    let src = document.getElementById("eye").src;
+    var input = document.getElementById("PasswordInput");
+    if(src=="http://localhost:63342/keywave/src/visible.png"){
+        document.getElementById("eye").src = "../src/hide.png";
+        input.type = "text";
+    }
+    else if(src=="http://localhost:63342/keywave/src/hide.png"){
+        document.getElementById("eye").src = "../src/visible.png";
+        input.type = "password";
+    }
+}
+function Email(){
+    let text = document.getElementById("EmailInput").value;
+    if(regex.test(text)){
+        const image = document.getElementById('mail');
+        if(image.src!='http://localhost:63342/keywave/src/check.png') {
+            image.classList.add('hidden');
+            setTimeout(() => {
+                image.src = 'http://localhost:63342/keywave/src/check.png';
+                image.classList.remove('hidden');
+            }, 250);
+        }
+    }
+    else if(!regex.test(text)){
+        const image = document.getElementById('mail');
+        if(image.src!='http://localhost:63342/keywave/src/mail.png') {
+            image.classList.add('hidden');
+            setTimeout(() => {
+                image.src = 'http://localhost:63342/keywave/src/mail.png';
+                image.classList.remove('hidden');
+            }, 250);
+        }
+    }
+}
 document.getElementById("ResetPassword").addEventListener('click',()=>{
     location.href = "./restorePassword.php";
 });
--- sites/login.php
+++ sites/login.php
@@ -7,8 +7,6 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login - Keywave</title>
     <link rel="stylesheet" href="../styles/loginStyle.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
-    <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/inter-ui@3.11.0/inter.min.css'>
 </head>
 <body>
 <header>
@@ -29,31 +27,18 @@
 <div class="loginBox">
     <form method="post">
         <h1>Login</h1>
-        <div class="email-input">
-            <svg viewBox="0 0 18 18">
-                <path d="M11.5,10.5 C6.4987941,17.5909626 1,3.73719105 11.5,6 C10.4594155,14.5485365 17,13.418278 17,9 C17,4.581722 13.418278,1 9,1 C4.581722,1 1,4.581722 1,9 C1,13.418278 4.581722,17 9,17 C13.418278,17 17,13.42 17,9"></path>
-                <polyline points="5 9.25 8 12 13 6"></polyline>
-            </svg>
-            <input type="email" placeholder="example@example.pl">
+        <div class="password-field">
+            <button type="button">
+                <img src="../src/mail.png" alt="mail" style="width: 25px;" id="mail">
+            </button>
+            <input type="email" placeholder="example@example.com" id="EmailInput" onchange="Email()">
         </div>
         <br>
         <div class="password-field">
             <button type="button">
-                <svg viewBox="0 0 21 21">
-                    <circle class="eye" cx="10.5" cy="10.5" r="2.25" />
-                    <path class="top" d="M2 10.5C2 10.5 6.43686 5.5 10.5 5.5C14.5631 5.5 19 10.5 19 10.5" />
-                    <path class="bottom" d="M2 10.5C2 10.5 6.43686 15.5 10.5 15.5C14.5631 15.5 19 10.5 19 10.5" />
-                    <g class="lashes">
-                        <path d="M10.5 15.5V18" />
-                        <path d="M14.5 14.5L15.25 17" />
-                        <path d="M6.5 14.5L5.75 17" />
-                        <path d="M3.5 12.5L2 15" />
-                        <path d="M17.5 12.5L19 15" />
-                    </g>
-                </svg>
+                <img onclick="Eye()" src="../src/visible.png" alt="eye" style="width: 25px" id="eye">
             </button>
-            <input type="password" placeholder="Password">
-            <input class="clear" type="text" placeholder="Password">
+            <input type="password" placeholder="Password" id="PasswordInput">
         </div>
         <br><br>
         <input type="submit" value="Login">
@@ -61,7 +46,6 @@
     </form>
 <div class="login">
 </center>
-<script src='https://unpkg.co/gsap@3/dist/gsap.min.js'></script>
-<script src='https://assets.codepen.io/16327/MorphSVGPlugin3.min.js'></script><script  src="../javascript/loginJS.js"></script>
+<script  src="../javascript/loginJS.js"></script>
 </body>
 </html>
