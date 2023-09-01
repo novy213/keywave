d5788cecc9dce5fdf165e4b4624a9dc4b2fdcc3c
novy213 <j.solarek@wp.pl>
2023-09-01 13:47:02 +0200

    fixed webbrowser type

--- sites/restorePassword.php
+++ sites/restorePassword.php
@@ -5,9 +5,6 @@ session_start();
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Login - Keywave</title>
     <link rel="stylesheet" href="../styles/restoreStyle.css">
 </head>
