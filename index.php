ee1b4e1c572e38691f40b43758a89b330d284cea
novy213 <j.solarek@wp.pl>
2023-09-03 11:17:49 +0200

    add simple index layout

--- index.php
+++ index.php
@@ -15,16 +15,40 @@ else{
     <title>Keywave</title>
 </head>
 <body>
+<center>
+    <div class="wavy">
+        <span style="--i:1">K</span>
+        <span style="--i:2">e</span>
+        <span style="--i:3">y</span>
+        <span style="--i:4">w</span>
+        <span style="--i:5">a</span>
+        <span style="--i:6">v</span>
+        <span style="--i:7">e</span>
+    </div>
+    <div class="container">
+        <form method="post">
+            <button class="Logout">Marketplace</button>
+            <button class="Logout">Wymiana live</button>
+            <button class="Logout">Moje konto</button>
+        </form>
+        <div class="item">
+            <p>test</p>
+        </div>
+    </div>
+</center>
 <?php
-echo $_SESSION['loged'];
+echo $_SESSION['loged']."<br>";
+$q = "select * from user;";
+$userInfo = mysqli_fetch_row(mysqli_query($db,$q));
+echo "witaj ".$userInfo[1]." ".$userInfo[2]."<br>";
 ?>
 <form method="post">
-    <input type="submit" name="submit">
+    <input type="submit" name="submit" value="Wyloguj">
 </form>
 <?php
-if(isset($_POST['submit'])){
+/*if(isset($_POST['submit'])){
     session_destroy();
-}
+}*/
 ?>
 <script src="javascript/indexJS.js"></script>
 </body>
